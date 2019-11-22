94c4ca100568dbe2c0b3415f85f72b20be54d8e1
lmy <alsdudwlwhs1@naver.com>
2019-11-22 20:17:16 +0900

    created an adapter and xml file for grid view.

--- app/src/main/java/com/lmy/smartkindergartencontroller/adapters/RecyclerAdapter.java
+++ app/src/main/java/com/lmy/smartkindergartencontroller/adapters/RecyclerAdapter.java
@@ -1,15 +1,73 @@
 package com.lmy.smartkindergartencontroller.adapters;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.lmy.smartkindergartencontroller.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerAdapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
     private static final String TAG = "RecyclerAdapter";
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerAdapter(ArrayList<String> mImages, Context mContext) {
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_griditem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clied on: " + position);
+                Toast.makeText(mContext, mImages.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView image;
+        private RelativeLayout parentLayout;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            parentLayout = itemView.findViewById(R.id.layout_parent);
         }
     }
 }
