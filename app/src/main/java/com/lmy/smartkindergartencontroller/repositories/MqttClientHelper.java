d2f2ce2e76e825e984f5343563e0b007ae0941ad
lmy <alsdudwlwhs1@naver.com>
2019-11-23 18:36:51 +0900

    created a mqtt helper class

---
+++ app/src/main/java/com/lmy/smartkindergartencontroller/repositories/MqttClientHelper.java
@@ -0,0 +1,133 @@
+package com.lmy.smartkindergartencontroller.repositories;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MqttClientHelper {
+
+    private static final String TAG = "MqttClientHelper";
+
+    private static MqttClientHelper mqttClientHelper;
+
+    private MqttAndroidClient mqttAndroidClient;
+
+    private Context mContext;
+
+    private String mMqttServerUri;
+    private String mMqttServerPort;
+    private String mSubscribeTopic;
+    private String mPublishTopic;
+    private String mPublishOnLed;
+    private String mPublishOnMotor;
+    private String mPublishOnTemp;
+    private String mPublishOnHumid;
+    private String mPublishOnUltra;
+    private String mPublishOnVideo;
+
+    private MqttClientHelper() {
+
+        mMqttServerUri = "tcp://mqtt.eclipse.org";
+        mMqttServerPort = "1883";
+        mSubscribeTopic = "korea/subscribe";
+        mPublishTopic = "korea/publish";
+        mPublishOnLed = "";
+        mPublishOnMotor = "";
+        mPublishOnTemp = "";
+        mPublishOnHumid = "";
+        mPublishOnUltra = "";
+        mPublishOnVideo = "";
+
+    }
+
+    public static MqttClientHelper getInstance() {
+        if(mqttClientHelper == null) {
+            mqttClientHelper = new MqttClientHelper();
+        }
+        return mqttClientHelper;
+    }
+
+    public void init(Context mContext) {
+
+        this.mContext = mContext;
+
+        mqttAndroidClient = new MqttAndroidClient(mContext, mMqttServerUri+":"+mMqttServerPort, MqttClient.generateClientId());
+
+        try {
+            IMqttToken token = mqttAndroidClient.connect(getMqttConnectionOption());
+
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "onSuccess: called.");
+                    mqttAndroidClient.setBufferOpts(getDisconnectedBufferOptions());    //연결에 성공한경우
+                    subscribe();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {   //연결에 실패한경우
+                    Log.d(TAG, "onFailure: " + exception.getMessage());
+                }
+            });
+        }
+        catch (MqttException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private DisconnectedBufferOptions getDisconnectedBufferOptions() {
+
+        DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+        disconnectedBufferOptions.setBufferEnabled(true);
+        disconnectedBufferOptions.setBufferSize(100);
+        disconnectedBufferOptions.setPersistBuffer(true);
+        disconnectedBufferOptions.setDeleteOldestMessages(false);
+
+        return disconnectedBufferOptions;
+    }
+
+
+    private MqttConnectOptions getMqttConnectionOption() {
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setWill("aaa", "I am going offline".getBytes(), 1, true);
+
+        return mqttConnectOptions;
+    }
+
+    private void subscribe() {
+        try {
+            mqttAndroidClient.subscribe(mSubscribeTopic, 0, new IMqttMessageListener() { // korea/lmy 구독
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception { // 구독 시 callback 함수
+                    String msg = new String(message.getPayload());
+                    Log.d(TAG, "messageArrived: " + msg);
+                }
+            });
+        }
+        catch (MqttException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(int position) {
+        try {
+            mqttAndroidClient.publish(mPublishTopic, Integer.toString(position).getBytes(), 0, false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
